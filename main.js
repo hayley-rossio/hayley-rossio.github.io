7153cfbb5a2ce0e43f1f93a948a68836388f223f
Hayley Rossio <hrossio@nmu.edu>
2025-03-14 13:40:34 -0400

    Nav bar link testing

---
+++ main.js
@@ -0,0 +1,8 @@
+function navBar() {
+    var x = document.getElementById("navBar");
+    if(x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
