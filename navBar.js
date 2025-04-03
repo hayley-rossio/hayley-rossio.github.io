348b6727487deda624b5d300bfbd694063f22640
Hayley Rossio <hrossio@nmu.edu>
2025-04-03 01:23:42 -0400

    Nav bar work.

--- navBar.js
+++ navBar.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navbar = `
         <nav>
-            <ul>
-                <li><a href="">{{username}}'s Home</a></li>
-                <li><a href="">Workout Input</a></li>
-                <li><a href="">Profile</a></li>
-                <li><a href="">Equipment</a></li>
-            </ul>
+            <a href="main.html">{{username}}'s Home</a> | 
+            <a href="workout.html">Workout Input</a> | 
+            <a href="proflie.html">Profile</a> |
+            <a href="equip.html">Equipment</a> 
         </nav>
     `;
     document.getElementById("navbar-container").innerHTML = navbar;
