deceb1542e7694c07259de4e5779db2e06a59571
Hayley Rossio <hrossio@nmu.edu>
2025-04-03 01:27:59 -0400

    Created all html, and add navBar to each.

--- navBar.js
+++ navBar.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navbar = `
         <nav>
-            <a href="main.html">{{username}}'s Home</a> | 
+            <a href="main.html">Home</a> | 
             <a href="workout.html">Workout Input</a> | 
             <a href="proflie.html">Profile</a> |
             <a href="equip.html">Equipment</a> 
