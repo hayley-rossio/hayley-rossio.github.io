4a8aaf0b59b87dcd4c7348588dc312d2aca20eb6
Hayley Rossio <hrossio@nmu.edu>
2025-04-03 01:16:51 -0400

    First commit

--- main.js
+++
@@ -1,8 +0,0 @@
-function navBar() {
-    var x = document.getElementById("navBar");
-    if(x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
